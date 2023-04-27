0e689af0d31f099b376d35be8e008064ac0ade5c
Райымбек <@raiymb>
2023-04-28 00:07:16 +0600

    Add given code

---
+++ src/HashNode.java
@@ -0,0 +1,15 @@
+public class HashNode<K,V> {
+    private K key;
+    private V value;
+    private HashNode<K,V> next;
+
+    public HashNode(K key, V value){
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + key + " " + value + "}";
+    }
+}
---
+++ src/MyHashTable.java
@@ -0,0 +1,37 @@
+public class MyHashTable<K,V> {
+    private class HashNode<K,V>{}
+    private int CONST_SIZE = 11;
+    private int size;
+
+    public MyHashTable(){
+
+    }
+
+    public MyHashTable(int CONST_SIZE){
+
+    }
+
+    private int hash(K key){
+
+    }
+
+    public void put(K key, V value){
+
+    }
+
+    public V get(K key){
+
+    }
+
+    public V remove(K key){
+
+    }
+
+    public boolean contains(V value){
+
+    }
+
+    public K getKey(V value){
+        
+    }
+}
